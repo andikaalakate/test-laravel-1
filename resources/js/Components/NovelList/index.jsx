1b4dd60e8614e31a9e81d1d46b47b350aedb50e3
Andika Alakate <andikaalakate@gmail.com>
2024-01-12 15:18:09 +0700

    Menambahkan AOS

--- resources/js/Components/NovelList/index.jsx
+++ resources/js/Components/NovelList/index.jsx
@@ -4,10 +4,17 @@ import Card from "./Card";
 const NovelList = ({hTitle, novels}) => {
     return (
         <>
-        <h3 className="text-4xl py-8 font-bold text-center mx-auto text-color-dark">{hTitle}</h3>
-        <div className="animate__animated animate__fadeInUp animate__slow row justify-center mini:grid-cols-2 my-4 grid mini:gap-6 hmin:grid-cols-2 sm:grid-cols-3 sm:gap-4 md:grid-cols-4 lg:grid-cols-5 mx-auto max-w-5xl">
-            <Card novels={novels} />
-        </div>
+            <h3 data-aos="fade-up"
+                data-aos-anchor-placement="top-center" className="mx-auto py-8 text-center text-4xl font-bold text-color-dark">
+                {hTitle}
+            </h3>
+            <div
+                data-aos="fade-up"
+                data-aos-anchor-placement="top-center"
+                className="row mx-auto my-4 grid max-w-5xl justify-center mini:grid-cols-2 mini:gap-6 hmin:grid-cols-2 sm:grid-cols-3 sm:gap-4 md:grid-cols-4 lg:grid-cols-5"
+            >
+                <Card novels={novels} />
+            </div>
         </>
     );
 };
--- resources/js/Pages/Page.jsx
+++ resources/js/Pages/Page.jsx
@@ -3,8 +3,14 @@ import AboutVanta from "@/Components/Contents/AboutVanta/index.jsx";
 import Navbar from "@/Components/Navbar/index.jsx";
 import NovelList from "@/Components/NovelList/index.jsx";
 import Layout from "@/Layouts/Layout";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const Page = ({novels, auth}) => {
+    useEffect(() => {
+        AOS.init({ duration: 1000 });
+    });
     const images = [];
 
     for (let i = 1; i <= 10; i++) {
@@ -17,24 +23,32 @@ const Page = ({novels, auth}) => {
     }
 
     return (
-      <>
-        <Layout title="Home">
-            <div className="min-h-screen bg-color-whity">
-                <Carousel images={images} />
-                <Navbar user={auth.user} />
-                <div className="m-4 mx-auto max-w-5xl rounded-lg border-2 p-4 text-black shadow-lg bg-white">
-                    <h1 className="text-center text-3xl font-bold">
-                        VANTA Translation
-                    </h1>
-                    <AboutVanta />
+        <>
+            <Layout title="Home">
+                <div className="min-h-screen bg-color-whity">
+                    <Carousel images={images} />
+                    <Navbar user={auth.user} />
+                    <div
+                        data-aos="fade-up"
+                        data-aos-anchor-placement="top-center"
+                        className="m-4 mx-auto max-w-5xl rounded-lg border-2 bg-white p-4 text-black shadow-lg"
+                    >
+                        <h1
+                            data-aos="fade-up"
+                            data-aos-anchor-placement="top-center"
+                            className="text-center text-3xl font-bold"
+                        >
+                            VANTA Translation
+                        </h1>
+                        <AboutVanta />
+                    </div>
+                    <div className="mx-auto p-4 pb-20">
+                        <NovelList hTitle="Novel Terbaru" novels={novels} />
+                        <NovelList hTitle="Novel Populer" novels={novels} />
+                    </div>
                 </div>
-                <div className="mx-auto p-4 pb-20">
-                    <NovelList hTitle="Novel Terbaru" novels={novels} />
-                    <NovelList hTitle="Novel Populer" novels={novels} />
-                </div>
-            </div>
-        </Layout>
-      </>
+            </Layout>
+        </>
     );
 };
 
